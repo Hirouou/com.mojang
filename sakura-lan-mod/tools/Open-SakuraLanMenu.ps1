c3217d7277cfd8a70976299d9ab7ccd74a2bec60
Hirouou <162318970+Hirouou@users.noreply.github.com>
2026-09-25 06:13:57 -0300

    Add direct shortcut to Sakura LAN client menu

---
+++ sakura-lan-mod/tools/Open-SakuraLanMenu.ps1
@@ -0,0 +1,11 @@
+param([ValidateSet('Host', 'Client')][string]$Instance = 'Client')
+$ErrorActionPreference = 'Stop'
+$adb = 'D:\SakuraLAN\sdk\platform-tools\adb.exe'
+$serial = if ($Instance -eq 'Host') { 'emulator-5554' } else { 'emulator-5556' }
+if (-not ((& $adb devices) -match "$serial\s+device")) {
+    throw "$Instance não está aberto. Use Iniciar teste Sakura primeiro."
+}
+& $adb -s $serial shell am force-stop jp.garud.ssimulator
+& $adb -s $serial shell am start -n jp.garud.ssimulator/jp.garud.ssimulator.SakuraLanActivity
+if ($LASTEXITCODE -ne 0) { throw "Falha ao abrir SAKURA LAN em $serial" }
+Write-Host "Menu SAKURA LAN aberto em $Instance."
