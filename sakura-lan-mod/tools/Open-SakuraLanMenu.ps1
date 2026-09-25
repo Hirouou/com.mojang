06056a2b1542362e9769df55a4b7b0c6f3594e91
Hirouou <162318970+Hirouou@users.noreply.github.com>
2026-09-25 06:28:31 -0300

    Prefill emulator Host IP when opening Sakura LAN client menu

--- sakura-lan-mod/tools/Open-SakuraLanMenu.ps1
+++ sakura-lan-mod/tools/Open-SakuraLanMenu.ps1
@@ -6,6 +6,10 @@ if (-not ((& $adb devices) -match "$serial\s+device")) {
     throw "$Instance não está aberto. Use Iniciar teste Sakura primeiro."
 }
 & $adb -s $serial shell am force-stop jp.garud.ssimulator
-& $adb -s $serial shell am start -n jp.garud.ssimulator/jp.garud.ssimulator.SakuraLanActivity
+if ($Instance -eq 'Client') {
+    & $adb -s $serial shell am start -n jp.garud.ssimulator/jp.garud.ssimulator.SakuraLanActivity --es sakuralan_host 10.0.2.2 --ei sakuralan_port 38556
+} else {
+    & $adb -s $serial shell am start -n jp.garud.ssimulator/jp.garud.ssimulator.SakuraLanActivity
+}
 if ($LASTEXITCODE -ne 0) { throw "Falha ao abrir SAKURA LAN em $serial" }
 Write-Host "Menu SAKURA LAN aberto em $Instance."
